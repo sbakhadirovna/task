ef266bc18c9a16193b179bc792d1012f14f1a558
sbakhadirovna <s.bakhadirovna@gmail.com>
2024-03-28 21:29:49 +0500

    2.11-masala

---
+++ 2.11-masala.js
@@ -0,0 +1,16 @@
+const main =()=> {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+            continue;
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+            continue;
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+            continue;
+        }
+        console.log(i);
+    }
+};
+main();
